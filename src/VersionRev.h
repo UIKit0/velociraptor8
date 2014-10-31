7aa325838bd0b8036e4060a0ada69a0828c7ccf7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-10-30 20:18:59 -0700

    update VersionRev.h and don't run the script that updates the version from the build

--- src/VersionRev.h
+++ src/VersionRev.h
@@ -1,3 +1,4 @@
-#define VERSION_HASH _T("f7766c8")
-#define VERSION_REV 964
-#define VERSION_REV_FULL _T("964 (f7766c8)")
+/* This file is only so that during pre-release build we can
+#define PRE_RELEASE_VER $n
+here, where $n is number of git commits.
+*/
