5888d069ce8beb4d1c5b63bace5271666cee01ed
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2014-11-29 00:53:25 -0800

    update project for using sumatra's utils

--- gyp/gen.bat
+++ gyp/gen.bat
@@ -1,4 +1,4 @@
 @rem This script assumes that gyp is checked out alongside sumatrapdf directory
-@rem and it's being called as: gype\gen.bat
+@rem and it's being called as: gyp\gen.bat
 
 call ..\gyp\gyp.bat -G msvs_version=2013 -f msvs --depth=. -Igyp\common.gypi gyp\velociraptor8.gyp
